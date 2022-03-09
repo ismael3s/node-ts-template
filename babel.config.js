62f97a44f45481433a7850b14f452fb4543c5b44
Ismael Santana <souz4ismael@gmail.com>
2022-03-09 11:19:18 -0300

    added babel for build

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@modules': ['./src/modules'],
+          '@utils': ['./src/utils'],
+          '@shared': ['./src/shared'],
+          '@config': ['./src/config'],
+          '@errors': ['./src/errors'],
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
---
+++ src/modules/generic/useCases/createGeneric/CreateGenericUseCase.spec.ts
@@ -0,0 +1,3 @@
+describe("Create Generic Use Case", () => {
+    
+})
